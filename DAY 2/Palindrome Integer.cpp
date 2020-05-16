521efffbf2411dc5a63e7bac2b09106ccfe00ed3
Piyush Khurana <piyushkhurana165@gmail.com>
2020-05-17 04:15:44 +0530

    Create Palindrome Integer.cpp

---
+++ DAY 2/Palindrome Integer.cpp
@@ -0,0 +1,19 @@
+// Question: https://www.interviewbit.com/problems/palindrome-integer/
+// TC: O(n)
+
+
+int Solution::isPalindrome(int A) {
+    if(A < 0)
+        return false;
+    
+    string s = to_string(A);
+    int start = 0, end = s.size() - 1;
+    while(start < end){
+        if(s[start] != s[end])
+            return false;
+        
+        start++;
+        end--;
+    }
+    return true;
+}
