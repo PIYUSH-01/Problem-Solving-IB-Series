e40404f4a124150788c2bed7ebd1ec12f025d79b
Piyush Khurana <piyushkhurana165@gmail.com>
2020-05-17 03:24:45 +0530

    Create Excel Column Number.cpp

---
+++ DAY 2/Excel Column Number.cpp
@@ -0,0 +1,14 @@
+// Question link: https://www.interviewbit.com/problems/excel-column-number/
+// TC: O(n)
+
+// Basically in this question we have used the same approach that we use in conversions like if we convert a bnary to decimal.
+
+int Solution::titleToNumber(string A) {
+    
+    int sum = 0, j = 0;
+    for(int i=A.size() - 1; i >= 0; i--){
+        sum = sum +(A[i] - 'A' + 1) * pow(26, j);
+        j++;
+    }
+    return sum;
+}
