a9175b87d9bc7e2e6004a8af59a0107b85c6707c
Piyush Khurana <piyushkhurana165@gmail.com>
2020-05-17 00:25:14 +0530

    Added 2nd ques

---
+++ DAY 1/Sum of pairwise Hamming Distance.cpp
@@ -0,0 +1,23 @@
+// Question: https://www.interviewbit.com/problems/sum-of-pairwise-hamming-distance/
+// Timecomplexity: O(n)
+
+#define MOD 1000000007
+#define ll long long
+int Solution::hammingDistance(const vector<int> &A) {
+    ll ans=0;
+    int n=A.size();
+    for(int i =0; i< 31; i++){
+        ll cnt0 = 0;
+        ll cnt1 = 0;
+        for(int j = 0; j < n; j++){
+            if(A[j] &( 1 << i))
+                cnt1++;
+            else
+                cnt0++;
+        }
+        ll temp = (cnt0%MOD * (cnt1%MOD))%MOD;
+        temp = (temp%MOD *2) %MOD;
+        ans = (ans%MOD + temp%MOD) %MOD;
+    }
+    return (int)ans;
+}
