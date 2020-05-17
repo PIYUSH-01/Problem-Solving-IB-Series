705d287a051cdf9bbca3c6ba9b30a7cd20d39f08
Piyush Khurana <piyushkhurana165@gmail.com>
2020-05-17 06:49:39 +0530

    Update Greatest Common Divisor.cpp

--- DAY 2/Greatest Common Divisor.cpp
+++ DAY 2/Greatest Common Divisor.cpp
@@ -1,6 +1,6 @@
 // TC:
 
-// Approach: Here we have used the inbuilt GCD function but you cannot use it i interview so keep in mind to implement the GCD function by yourself.
+// Approach: Here we have not used the inbuilt GCD function and you cannot use it in interview so keep in mind to implement the GCD function by yourself.
 
 int Solution::gcd(int A, int B) {
     if(B == 0)
